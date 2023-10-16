d8db1cf1143ffe1c0720282a7793e67aeff69f31
lechinal <alin.lechintan@outlook.com>
2023-10-16 20:58:02 +0300

    up

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -5,13 +5,27 @@ function Stats({ totalItems, checkedItems, remainingItems, totalQuantity }) {
   return (
     <div className={styles.statsBox}>
       <p className={styles.statsText}>
-        You have {totalItems} items in the list, with a Total quantity of:{' '}
-        {totalQuantity}
+        You have{' '}
+        <span style={{ fontWeight: 'bold', color: 'blue' }}>{totalItems}</span>{' '}
+        items in the list, with a Total quantity of:{' '}
+        <span style={{ fontWeight: 'bold', color: 'blue' }}>
+          {totalQuantity}
+        </span>
       </p>
-      <p className={styles.statsText}>You have {checkedItems} items checked.</p>
+      <p className={styles.statsText}>
+        You have{' '}
+        <span style={{ fontWeight: 'bold', color: 'blue' }}>
+          {checkedItems}
+        </span>{' '}
+        items checked.
+      </p>
       <p className={styles.statsText}>
         {' '}
-        You have {remainingItems} remaining items to be checked.
+        You have{' '}
+        <span style={{ fontWeight: 'bold', color: 'blue' }}>
+          {remainingItems}
+        </span>{' '}
+        remaining items to be checked.
       </p>
     </div>
   );
